6ebf56106f6b71e81661bbafce8f3e9c6742844c
Josep Palomino <jfpalomino.22@gmail.com>
2021-11-18 10:27:25 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React :D
+          Learn React en JF5 Store
         </a>
       </header>
     </div>
